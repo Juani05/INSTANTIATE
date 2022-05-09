68400ec91edba1a5f812865f7850f8aeec36c50e
Juani05 <102042211+Juani05@users.noreply.github.com>
2022-05-09 10:42:01 -0300

    Finished exercise with cube

---
+++ Assets/Scripts/instantiator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    public void CloneObject()
+    {
+        int contador = 0;
+        while (contador < 3)
+        {
+            Instantiate(objectToClone);
+            contador++;
+        }
+        }
+}
