fb99f8dfd596dc21c7d3e01465cdc6da96f4bcc0
Juani05 <102042211+Juani05@users.noreply.github.com>
2022-05-10 10:46:27 -0300

    Finished exercise inputmanager

---
+++ Assets/Scripts/inputmanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputmanager : MonoBehaviour
+{
+    public InputField ifusername;
+    public Text txtusermanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UserGreetings ()
+    {
+        Debug.Log(int.Parse(txtusermanager.text) * 2);
+    }
+}
